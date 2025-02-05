6cbc23dbf6342a6c672f001a5af34a3a320d4f4c
AZİZE SULTAN PALALI <azizebilge2@gmail.com>
2025-02-05 16:05:48 +0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
                               title=f"Gantt Chart - {domain}", text="Task", hover_data=["Sub Domain", "Subject Area", "Task"])
             fig.update_traces(marker=dict(line=dict(width=2, color='rgba(0,0,0,0.3)')), textposition='inside')  # Gölge efekti eklendi
             fig.update_yaxes(categoryorder="total ascending", showgrid=True, visible=True)
-            fig.update_layout(bargap=0.1, plot_bgcolor='white', paper_bgcolor='white', 
+            fig.update_layout(bargap=0.1, 
                 autosize=True,
                 height=1000,  # Grafiğin dikey boyutunu artırdım
                 width=2200,
